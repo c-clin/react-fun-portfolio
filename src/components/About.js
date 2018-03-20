01bde0af1ee1028694c5bfe0baebf50edca41675
Catherine Lin <catherine.lin.93@gmail.com>
2018-03-20 02:12:33 -0700

    unsplash api is working

--- src/components/About.js
+++ src/components/About.js
@@ -23,31 +23,33 @@ class About extends Component {
   getCat() {
     console.log('click');
     $.getJSON(finalURL).done(function(data) {
-      var result = data.id;
+      var resultPhoto = data.urls.thumb;
       var user = data.user.name;
-      this.setState({resultCat: result, resultUser: user}, function() {
+      this.setState({resultCat: resultPhoto, resultUser: 'Photo by: ' + user}, function() {
         console.log(this.state);
       });
-      // console.log(result, user);
-    }.bind(this));
+    }.bind(this))
+    .fail(function() {
+      console.log('There was an error loading the Unsplash API');
+    })
   }
-
-
-  // componentDidMount() {
-  //  this.getCat();
-  // }
 
   render() {
     return (
       <div className="about">
-        <div>
-        <h3>About Me</h3>
+          <h3>About Me</h3>
 
-        <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel dolor id leo hendrerit sodales vitae ut felis. Nam viverra tellus posuere, elementum sem nec, scelerisque metus. Morbi at mauris eu lectus lacinia placerat sed dapibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla scelerisque orci, eget ornare nibh lacinia vitae. Sed pulvinar est quis orci vulputate imperdiet. Phasellus condimentum nisl nec tempus suscipit. Curabitur tristique nunc at enim iaculis suscipit. Duis eu dui a enim egestas tristique. Aliquam id sem interdum, faucibus purus eu, rutrum urna. Vivamus in mauris lacus. Nunc at mi non lacus fermentum feugiat quis vel elit. Duis dapibus eros lectus, ac suscipit quam sodales et.
-        </p>
-        <button onClick={this.getCat.bind(this)}>Cat</button>
-        </div>
+          <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel dolor id leo hendrerit sodales vitae ut felis. Nam viverra tellus posuere, elementum sem nec, scelerisque metus. Morbi at mauris eu lectus lacinia placerat sed dapibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla scelerisque orci, eget ornare nibh lacinia vitae. Sed pulvinar est quis orci vulputate imperdiet. Phasellus condimentum nisl nec tempus suscipit. Curabitur tristique nunc at enim iaculis suscipit. Duis eu dui a enim egestas tristique. Aliquam id sem interdum, faucibus purus eu, rutrum urna. Vivamus in mauris lacus. Nunc at mi non lacus fermentum feugiat quis vel elit. Duis dapibus eros lectus, ac suscipit quam sodales et.
+          </p>
+
+          <button onClick={this.getCat.bind(this)}>Get cat!</button>
+
+          <figure className="randomCatPhoto">
+            <img src={this.state.resultCat} />
+            <figcaption>{this.state.resultUser}</figcaption>
+          </figure>
+
       </div>
     );
   }
