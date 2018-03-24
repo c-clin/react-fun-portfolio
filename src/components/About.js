a91c2d1aac51a2a7016b4408075049b7b29ffe36
Catherine Lin <catherine.lin.93@gmail.com>
2018-03-24 02:20:22 -0700

    Removed unsplash api to use it for another project

--- src/components/About.js
+++ src/components/About.js
@@ -11,26 +11,6 @@ var finalURL = `https://api.unsplash.com/photos/random?&query=${query}&client_id
 
 class About extends Component {
 
-    constructor() {
-    super();
-    this.state = {
-      resultCat: [],
-      resultUser: []
-    };
-    this.getCat = this.getCat.bind(this);
-  }
-
-  getCat() {
-    $.getJSON(finalURL).done(function(data) {
-      var resultPhoto = data.urls.small;
-      var user = data.user.name;
-      this.setState({resultCat: resultPhoto, resultUser: 'Awesome photo by: ' + user});
-    }.bind(this))
-    .fail(function(error) {
-      throw new Error('fail(): ' + error);
-    })
-  }
-
   render() {
     return (
       <div className="about">
@@ -41,15 +21,6 @@ class About extends Component {
             Hi there! My name is Catherine and I'm a self-taught web developer in Los Angeles, California. I graduated from UCLA with a degree in Economics only to realize that my true passion lies in web development. I love exploring various frameworks and building amazing tools with clean, easy-to-read code. I'm currently seeking a Front-End Developer role in the greater Los Angeles area. I'd love to join a team of passionate and dedicated individuals who value hard work and enjoy the grind. Feel free to check out more of my work on Github and connect with me on Linkedin!          </p>
 
         </div>
-
-          <div className="unsplash">
-            <button onClick={this.getCat.bind(this)}>get cat!</button>
-            <figure className="randomCatPhoto">
-              <img src={this.state.resultCat} />
-              <figcaption>{this.state.resultUser}</figcaption>
-            </figure>
-          </div>
-
 
       </div>
     );
